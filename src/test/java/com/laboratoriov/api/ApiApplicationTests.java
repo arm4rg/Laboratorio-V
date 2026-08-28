a9ad75a6c3a925dde59dbf404da2a0e05c23d15b
arm4rg <larmandoreyes502@gmail.com>
2026-08-28 17:07:46 -0600

    Creacion del Repositorio

---
+++ src/test/java/com/laboratoriov/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboratoriov.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
